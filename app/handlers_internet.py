d978873f1c0300ffbd4f3ca9a7f3bf2ef7ee5658
Wosmerik Alexander <shurik1777@gmail.com>
2024-09-13 12:49:41 +0300

    add text

---
+++ app/handlers_internet.py
@@ -0,0 +1,5 @@
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+from app.handler_text import *
+
+r_int = Router()
--- run.py
+++ run.py
@@ -3,11 +3,17 @@ import logging
 from aiogram import Dispatcher
 from config_reader import bot
 from app.handlers_start import router
+from app.handlers_internet import r_int
+from app.handlers_iptv import r_iptv
+from app.handlers_ktv import r_ktv
 
 
 async def main():
     dp = Dispatcher()
     dp.include_router(router)
+    dp.include_router(r_int)
+    dp.include_router(r_iptv)
+    dp.include_router(r_ktv)
     await dp.start_polling(bot)
 
 
